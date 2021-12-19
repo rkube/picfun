8b835041257208c2b7f74e00a3a8a76eac5164ba
Ralph Kube <ralph_kube@gmx.net>
2021-12-19 18:35:30 -0500

    Testing latex

--- src/grids.jl
+++ src/grids.jl
@@ -4,15 +4,13 @@ export grid_1d, init_grid
 
 """
 `grid_1d` represents a periodic grid on ``[0:L]``.
-Field values are disretized on the grid points``x_n = n \times \Delta z``,
-where n=0,1,...Nz-1.
-
+Field values are disretized on the grid points ``x_n = n \\times \\Delta z``,
+where ``n=0,1,...N_z-1``.
 
 # Fields
 - Lz: Total length of the domain
 - Nz: Number of grid points
 - Δz: Distance between neighbouring grid points.
-
 """
 struct grid_1d
   Lz
